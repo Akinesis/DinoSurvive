c87674563595f420c9ad380f00849375213d7b17
Akinesis <joachim.vanoni@gmail.com>
2014-02-23 16:58:54 +0100

    modification de chunk
    Passage d’un Vector de cubes à un dictionnaire de String(clef) et
    Cube3D(value)

--- src/Modeles/Chunk.java
+++ src/Modeles/Chunk.java
@@ -1,35 +1,40 @@
 package Modeles;
 
+import java.util.HashMap;
 import java.util.Vector;
+
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
+
 import Modeles.entities.*;
 
 public class Chunk {
-	private Vector<Cube3D> cubes;
+	private HashMap<String, Cube3D> cubes;
 	private Vector<Cube3D> renderCubes;
-	
+
 	public Chunk(){
-		cubes = new Vector<Cube3D>();
+		cubes = new HashMap<String, Cube3D>();
 		renderCubes = new Vector<Cube3D>();
 	}
-	
+
 	public void addCube(){
-		cubes.add(new Cube3D(0, 0, -5, 1));
+		cubes.put("1", new Cube3D(0, 0, -5, 1));
 	}
-	
+
 	private void checkState(){
-		for(Cube3D cube : cubes){
+		Vector<Cube3D> liste = new Vector<Cube3D>(cubes.values());
+		for(Cube3D cube : liste){
 			if(cube.getState()){
 				renderCubes.add(cube);
 			}
 		}
 	}
-	
+
 	private void genCubes(){
 		for(Cube3D cube : renderCubes){
 			cube.genCube();
 		}
 	}
-	
+
 	public void draw(){
 		checkState();
 		genCubes();
@@ -37,7 +42,7 @@ public class Chunk {
 			cube.draw();
 		}
 	}
-	
+
 	public void clearChunk(){
 		renderCubes.clear();
 	}
