087f96d64a8164edd74621572fe8b109b9b7cf1c
Freyj <aerandil@gmail.com>
2014-08-11 18:13:31 +0200

    Stupid commit
    je hais le picking

--- src/modeles/PickingRay.java
+++ src/modeles/PickingRay.java
@@ -2,15 +2,23 @@ package modeles;
 
 import org.lwjgl.util.vector.Vector3f;
 
+
 public class PickingRay 
 {
 	private Vector3f clickPosInWorld = new Vector3f();
 	private Vector3f direction = new Vector3f();
- 
+	
+	public PickingRay(Vector3f vec, Vector3f vec2){
+		clickPosInWorld = vec;
+		direction = vec2;
+		
+	}
 	/**
 	 * Computes the intersection of this ray with the X-Y Plane (where Z = 0)
 	 * and writes it back to the provided vector.
 	 */
+
+	
 	public void intersectionWithXyPlane(float[] worldPos)
 	{
 		float s = -clickPosInWorld.z / direction.z;
