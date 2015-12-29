caa8a426c6fed5ac51a53d4520e90411c9373907
Freyj <aerandil@gmail.com>
2015-12-29 17:36:25 +0100

    Suppression de la bdd (y a plus de références normalement)

--- src/modeles/ItemManager.java
+++ src/modeles/ItemManager.java
@@ -1,8 +1,6 @@
 package modeles;
 
 
-import java.sql.*;
-import java.sql.SQLException;
 
 import modeles.entities.Cube3dVbo;
 import modeles.entities.FlatItemVBO;
@@ -13,16 +11,10 @@ public class ItemManager {
 	
 	Controleur clone;
 	DropManager dropManager;
-	private BDDConector database;
 	
 	public ItemManager(Controleur contr){
 		clone = contr;
 		dropManager = new DropManager();
-		database = new BDDConector();
-		
-		coonectBDD();
-		
-		stopBDD();
 	}
 	
 	public void addDropOnFoot(Item item){
@@ -34,42 +26,8 @@ public class ItemManager {
 		FlatItemVBO temp = new FlatItemVBO(cube.getX()+0.5f, cube.getY(), cube.getZ()-0.5f, item.getIdentifiant()); //à modifier un peut.
 		dropManager.addDrop(temp);
 	}
-	
-	private void coonectBDD(){
-		try {
-			database.connexionBDD();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	private void stopBDD(){
-		database.arretBDD();
-	}
-	
+
 	public void createItem(int id){
-		PreparedStatement prepStatement;
-		try {
-			prepStatement = database.getConnetion().prepareStatement("SELECT* FROM DINODATA.items WHERE ID=?");
-			prepStatement.setInt(1,id);
-			
-			ResultSet res = prepStatement.executeQuery();
-			
-			while(res.next()){
-				String nature = res.getString("nature"); //etc...
-				/*
-				 * TO DO :
-				 * écrire la création d'objet içi une fois les obhjets définits.
-				 */
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 		
 	}
 	
